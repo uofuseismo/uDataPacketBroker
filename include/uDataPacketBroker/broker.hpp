7ef5a97117b4d65ad01701dcd1c73710af435ae2
Ben Baker <bakerb845@gmail.com>
2026-07-24 11:24:36 -0600

    pulling latest api for claude

---
+++ include/uDataPacketBroker/broker.hpp
@@ -0,0 +1,50 @@
+#ifndef UDATA_PACKET_BROKER_BROKER_HPP
+#define UDATA_PACKET_BROKER_BROKER_HPP
+#include <chrono>
+#include <memory>
+#include <spdlog/logger.h>
+namespace UDataPacketBroker
+{
+ class BrokerOptions;
+}
+namespace UDataPacketBroker
+{ 
+/// @class Broker broker.hpp
+/// @brief This is the application middleware.  It basically does all the work.
+///        It receives data from the publisher clients and propagates them to the
+///        subscriber clients whilst updating the RocksDB.
+/// @copyright Ben Baker (University of Utah) distributed under the MIT
+///            NO AI license.
+class Broker
+{
+public:
+    /// @brief Initializes the broker from the given options and logger.
+    Broker(const BrokerOptions &options,
+           std::shared_ptr<spdlog::logger> logger);
+
+    /// @result True indicates the broker is initialized.
+    [[nodiscard]] bool isInitialized() const noexcept;
+
+    /// @brief Starts the broker.
+    /// @note You only get one shot.
+    void start();
+
+    /// @brief Management routine to check the running threads in the broker.
+    [[nodiscard]] bool checkFutures(const std::chrono::milliseconds &timeOut = std::chrono::milliseconds {10}) const noexcept;
+
+    /// @brief Stops the broker.
+    void stop();
+
+    /// @brief Destructor.
+    ~Broker();
+
+    Broker(const Broker &) = delete;
+    Broker(Broker &&) noexcept = delete;
+    Broker& operator=(const Broker &broker) = delete;
+    Broker& operator=(Broker &&broker) noexcept = delete;    
+private:
+    class BrokerImpl;
+    std::unique_ptr<BrokerImpl> pImpl;
+};
+}
+#endif
--- include/uDataPacketBroker/utilities.hpp
+++ include/uDataPacketBroker/utilities.hpp
@@ -1,6 +1,7 @@
 #ifndef UDATA_PACKET_BROKER_UTILITIES_HPP
 #define UDATA_PACKET_BROKER_UTILITIES_HPP
 #include <chrono>
+#include <expected>
 #include <string>
 namespace UDataPacketBrokerAPI::V1
 {
@@ -13,6 +14,14 @@ namespace UDataPacketBroker::Utilities
 
 /// @result The current time in microseconds or nanoseconds.
 template<typename T> T getNow();
+
+/// @brief Converts a stream identifier to a string representation like 
+///        UU.ELU.EHZ.01.
+[[nodiscard]] std::string toString(const UDataPacketBrokerAPI::V1::StreamIdentifier &identifier);
+/// @brief Validates a received data packet.
+/// @result True indicates the packet is okay otherwise provides
+///         a reason for why the packet is invalid.
+[[nodiscard]] auto isValid(const UDataPacketBrokerAPI::V1::Packet &packet) -> std::expected<bool, std::string>;
 
 }
 #endif
