760c53dc3f88e7e81bf4ba8343b5081a8064dd42
Ben Baker <bakerb845@gmail.com>
2026-07-24 08:17:37 -0600

    grabbing api

---
+++ conanfile.py
@@ -0,0 +1,69 @@
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import cmake_layout, CMake, CMakeDeps, CMakeToolchain
+
+class uDataPacketCacheConan(ConanFile):
+   name = "uDataPacketDataBroker"
+   #version = "0.0.1"
+   license = "MIT"
+   description = "A durable broker for seismic data packets."
+   url = "https://github.com/uofuseismo/uDataPacketDataBroker"
+   #topics = ("cache", "in-memory")
+   settings = "os", "compiler", "build_type", "arch"
+   options = {"build_tests" : [True, False],
+              "build_training" : [True, False],
+              "with_conan" : [True, False]}
+   default_options = {"hwloc/*:shared": "True",
+                      "opentelemetry-cpp/*:with_otlp_http": "True",
+                      "opentelemetry-cpp/*:with_otlp_grpc": "True",
+                      "opentelemetry-cpp/*:with_abi_v2" : "True",
+                      "spdlog/*:header_only" : "True",
+                      "build_tests" : "True",
+                      "build_training" : "False",
+                      "with_conan" : "True",}
+   export_sources = "CMakeLists.txt", "LICENSE", "README.md", "cmake/*", "src/*", "testing/*"
+   generators = "CMakeDeps", "CMakeToolchain"
+
+   def requirements(self):
+       # dependencies
+       self.requires("grpc/1.82.0")
+       self.requires("opentelemetry-cpp/1.26.0")
+       self.requires("protobuf/6.33.5")
+       self.requires("boost/1.91.0")
+       self.requires("spdlog/1.17.0")
+       self.requires("onetbb/2023.0.0")
+       self.requires("rocksdb/11.1.1")
+
+   def build_requirements(self):
+       # test dependncies and build tools
+       self.test_requires("catch2/3.15.2")
+
+   def layout(self):
+       # defines the project layout
+       cmake_layout(self)
+ 
+   def build(self):
+       # invokes the build system
+       cmake = CMake(self)
+       cmake.configure()
+       cmake.build()
+       #if can_run(self):
+       #   # run tests particularly CTest 
+       #   cmake.test()
+
+   def test(self):
+       if can_run(self):
+          cmake.test()
+
+   #def generate(self):
+   #    tc = CMakeToolchain(self)
+   #    tc.generate()
+
+   def package(self):
+       # copies files from the build to package folder
+       cmake = CMake(self)
+       cmake.install()
+
+   def package_info(self):
+       self.cpp_info.libs = ["uDataPacketDataBroker"]
+
